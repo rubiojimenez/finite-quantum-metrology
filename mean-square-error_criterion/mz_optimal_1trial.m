f943d1aa85077fbc4465a8d2b166a324fb9359b7
Jesús Rubio <53397884+jesus-rubiojimenez@users.noreply.github.com>
2021-07-18 21:59:52 +0100

    Quantum single-shot minimum mean square error

--- mean-square-error_criterion/mz_optimal_1trial.m
+++ mean-square-error_criterion/mz_optimal_1trial.m
@@ -1,26 +1,26 @@
 function [Sopt,sopt,soptvec_columns,bayes_bound,rho,pk,psik,rhobar,rhobarnew] = mz_optimal_1trial(initial_state,phase_width,phase_mean)
-% Optimal single-shot strategy, where ’initial_state’ is a pure state
-% for the Mach-Zehnder interferometer, ’phase_width’ is the width of the
-% parameter domain and ’phase_mean’ is its centre.
+% Optimal single-shot strategy, where 'initial_state' is a pure state
+% for the Mach-Zehnder interferometer, 'phase_width' is the width of the
+% parameter domain and 'phase_mean' is its centre.
 %
 % This programme calculates:
 %
-%   a) the optimal quantum estimator ’Sopt’
-%   b) the estimates ’sopt’ for the unknown parameter given by the
-%      spectrum of ’Sopt’
+%   a) the optimal quantum estimator 'Sopt'
+%   b) the estimates 'sopt' for the unknown parameter given by the
+%      spectrum of 'Sopt'
 %   c) the optimal projective measurement for a single trial given by
-%      the eigenvectors ’soptvec_columns’ of ’Sopt’
-%   d) the optimal single-shot mean square error ’bayes_bound’
-%   e) the zero-th quantum moment of the transformed density matrix ’rho’
-%   f) ’rho’ in its diagonal basis, denoted by ’pk’
-%   g) the matrix ’psik’ whose columns are the eigenvectors of ’rho’
-%   h) the first quantum moment of the transformed density matrix ’rhobar’
-%   i) ’rhobar’ in the eigenbasis of ’rho’, denoted by ’rhobarnew’
+%      the eigenvectors 'soptvec_columns' of 'Sopt'
+%   d) the optimal single-shot mean square error 'bayes_bound'
+%   e) the zero-th quantum moment of the transformed density matrix 'rho'
+%   f) 'rho' in its diagonal basis, denoted by 'pk'
+%   g) the matrix 'psik' whose columns are the eigenvectors of 'rho'
+%   h) the first quantum moment of the transformed density matrix 'rhobar'
+%   i) 'rhobar' in the eigenbasis of 'rho', denoted by 'rhobarnew'
 
-% Calculation of ’rho’ and ’rhobar’
+% Calculation of 'rho' and 'rhobar'
 index=1;
-kvec=zeros(1,length(initial_state)ˆ2);
-lvec=zeros(1,length(initial_state)ˆ2);
+kvec=zeros(1,length(initial_state)^2);
+lvec=zeros(1,length(initial_state)^2);
 for x1=1:sqrt(length(initial_state))
   for y1=1:sqrt(length(initial_state))
     for z1=1:sqrt(length(initial_state))
@@ -46,7 +46,7 @@ end
 
 kmat=sparse(vec2mat(kvec,sqrt(length(kvec))));
 lmat=sparse(vec2mat(lvec,sqrt(length(lvec))));
-initial_rho=kron(initial_state,initial_state’);175
+initial_rho=kron(initial_state,initial_state');
 rho=initial_rho.*kmat; rhobar=initial_rho.*lmat;
 rho=full(rho); rhobar=full(rhobar);
 
@@ -61,7 +61,7 @@ for x=1:length(pk)
 end
 
 % ’rhobar’ in the eigenbasis of ’rho’
-rhobarnew=psik’*rhobar*psik;
+rhobarnew=psik'*rhobar*psik;
 
 % Optimal single-shot strategy: projectors and outcomes
 Sopt_temp=zeros(length(pkvec),length(pkvec));
@@ -74,7 +74,7 @@ for a=1:length(pkvec)
 end
 
 Sopt_temp=sparse(Sopt_temp);
-Sopt=psik*Sopt_temp*psik’;
+Sopt=psik*Sopt_temp*psik';
 Sopt=full(Sopt);
 
 [soptvec_columns, sopt_temp] = eigs(Sopt,rank(Sopt));
@@ -86,7 +86,7 @@ soptvec_columns=sparse(soptvec_columns);
 sopt=sparse(sopt);
 if imag(sopt)<1e-5; sopt=real(sopt);
 else
-  error(’The estimates of the unknown parameter must be real. Check the cutoff in the intermediate calculations.’)
+  error('The estimates of the unknown parameter must be real. Check the cutoff in the intermediate calculations.')
 return
 end
 
@@ -97,7 +97,7 @@ phase=linspace(phase_mean-phase_width/2,phase_mean+phase_width/2,1000);
 bayes_bound=trapz(phase,phase.*phase)/phase_width - trace(Sopt*Sopt*rho);
 if imag(bayes_bound)<1e-10; bayes_bound=real(bayes_bound);
 else176
-  error(’The mean square error must be real. Check the cutoff in the intermediate calculations.’)
+  error('The mean square error must be real. Check the cutoff in the intermediate calculations.')
   return
 end
 end
