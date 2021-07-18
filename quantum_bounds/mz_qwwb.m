7c003e00066af85d155d9a11a5c4505d8b955964
Jesús Rubio <53397884+jesus-rubiojimenez@users.noreply.github.com>
2021-07-18 22:20:12 +0100

    Quantum Weiss-Weinstein bound

--- quantum_bounds/mz_qwwb.m
+++ quantum_bounds/mz_qwwb.m
@@ -1,7 +1,7 @@
 function [qwwb] = mz_qwwb(initial_state,phase_width,mu_max)
 % Quantum Weiss-Weinstein bound as a function of the number of
-% trials, where ’initial_state’ is a pure state for the Mach-Zehnder,
-% interferometer ’phase_width’ is the width of the parameter domain
+% trials, where 'initial_state' is a pure state for the Mach-Zehnder,
+% interferometer 'phase_width' is the width of the parameter domain
 % and ’mu_max’ is the maximum number of repetitions.
   
 % Space cutoff (for a single mode)
@@ -17,11 +17,11 @@ find_supremum=zeros(mu_max,length(h));
 qwwb=zeros(1,mu_max);
 for runs=1:mu_max
   for z=1:length(h)
-    zeta=initial_state’*phase_shift_diff(op_cutoff,h(z))*initial_state;
-    zeta2=initial_state’*phase_shift_diff(op_cutoff,2*h(z))*initial_state;
-    fid_function=abs(zeta)ˆ2;
-    find_supremum(runs,z)=h(z)ˆ2*(1-h(z)/W)ˆ2*fid_functionˆ(2*runs)/(2*fid_functionˆruns-2*(1-2*h(z)/W)*real(zetaˆ(2*runs)*conj(zeta2)ˆruns));
-end
-qwwb(runs)=max(find_supremum(runs,:));
+    zeta=initial_state'*phase_shift_diff(op_cutoff,h(z))*initial_state;
+    zeta2=initial_state'*phase_shift_diff(op_cutoff,2*h(z))*initial_state;
+    fid_function=abs(zeta)^2;
+    find_supremum(runs,z)=h(z)^2*(1-h(z)/W)^2*fid_function^(2*runs)/(2*fid_function^runs-2*(1-2*h(z)/W)*real(zeta^(2*runs)*conj(zeta2)^runs));
+  end
+  qwwb(runs)=max(find_supremum(runs,:));
 end
 end
