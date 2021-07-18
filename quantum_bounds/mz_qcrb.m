7800d954f2e1575ef912f1756f94f985507d1692
Jesús Rubio <53397884+jesus-rubiojimenez@users.noreply.github.com>
2021-07-18 22:18:34 +0100

    Quantum Cramér-Rao bound

--- quantum_bounds/mz_qcrb.m
+++ quantum_bounds/mz_qcrb.m
@@ -1,7 +1,7 @@
 function [qcrb] = mz_qcrb(initial_state,mu_max)
 % Quantum Cramer-Rao bound as a function of the number of trials, where
-% ’initial_state’ is a pure state for the Mach-Zehnder interferometer
-% and ’mu_max’ is the maximum number of repetitions.
+% 'initial_state' is a pure state for the Mach-Zehnder interferometer
+% and 'mu_max' is the maximum number of repetitions.
   
 % Number of repetitions
 observations=1:1:mu_max;
@@ -10,13 +10,13 @@ observations=1:1:mu_max;
 op_cutoff=sqrt(length(initial_state));
 
 % Quantum Fisher information (pure states and unitary encoding)
-expectation_n=initial_state’*j3schwinger(op_cutoff)*initial_state;
-expectation_n2=initial_state’*j3schwinger(op_cutoff)ˆ2*initial_state;
-qfi=4*(expectation_n2-expectation_nˆ2);
+expectation_n=initial_state'*j3schwinger(op_cutoff)*initial_state;
+expectation_n2=initial_state'*j3schwinger(op_cutoff)^2*initial_state;
+qfi=4*(expectation_n2-expectation_n^2);
 
 % Do we have information?
 if qfi==0
-  disp(’The Quantum Fisher information is zero.’)
+  disp('The Quantum Fisher information is zero.')
   return
 end
 
